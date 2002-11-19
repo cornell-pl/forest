f2579607c016a07864c195769e7f6e8e0a7b3c63
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2002-11-19 05:00:34 +0000

    fixed lib tests and added myrand

--- pads/padsc/libpads/tests/biglibtest1.c
+++ pads/padsc/libpads/tests/biglibtest1.c
@@ -68,7 +68,7 @@ int main(int argc, char** argv) {
       PDC_aint32_fw_read(pdc, &em, 6, &ed, &i1);
     }
     if (strncmp(argv[1], "norec", 5) == 0) {
-      if (PDC_ERR == PDCI_char_lit_scan(pdc, '\n', '\n', 1, 0, &bytes_skipped)) {
+      if (PDC_ERR == PDC_char_lit_scan(pdc, '\n', '\n', 1, 0, &bytes_skipped)) {
 	break;
       }
     } else {
