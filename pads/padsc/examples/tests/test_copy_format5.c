c514b13c30e85e0905a3d55668b5856ed3419bf3
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2003-05-07 17:33:17 +0000

    test file

---
+++ pads/padsc/examples/tests/test_copy_format5.c
@@ -0,0 +1,61 @@
+#include "libpadsc.h"
+#include "format5.h"
+
+int main(int argc, char** argv) {
+  PDC_t*          pdc;
+  call_ed         ced, cedCpy;
+  call            cdata, cdataCpy;
+
+  /* Open pdc handle */
+  if (PDC_ERR == PDC_open(&pdc, 0, 0)) {
+    error(2, "*** PDC_open failed ***");
+    exit(-1);
+  }
+
+  /* Open output file */
+  if (PDC_ERR == PDC_IO_fopen(pdc, "../data/ex_data.format5.cpy")) {
+    error(2, "*** PDC_IO_fopen failed ***");
+    exit(-1);
+  }
+
+  /*
+   * Try to read each line of data
+   */
+  while (!PDC_IO_at_EOF(pdc)) {
+    PDC_error_t res;
+    res= call_read(pdc, 0, &ced, &cdata);
+
+    if (res == PDC_OK) {
+      sfprintf(sfstdout, "Record okay:\t");
+      call_copy(pdc, &cdataCpy, &cdata);
+    } else {
+      sfprintf(sfstdout, "Record not okay:\t");
+      call_copy(pdc, &cdataCpy, &cdata);
+      call_ed_copy(pdc, &cedCpy, &ced);
+    }
+    sfprintf(sfstdout, "x = %d\t", cdata.x.x);
+    switch (cdata.pn.tag ){
+    case code : 
+	sfprintf(sfstdout, "tagged as code: %d\n",cdata.pn.val.code );
+	break;
+    case pn :
+	sfprintf(sfstdout, "tagged as phone number: %d\n", cdata.pn.val.pn);
+	break;
+    default:
+	sfprintf(sfstdout, "bogus tag. \n");
+	break;      
+    }
+  }
+
+  if (PDC_ERR == PDC_IO_close(pdc)) {
+    error(2, "*** PDC_IO_fclose failed ***");
+    exit(-1);
+  }
+
+  if (PDC_ERR == PDC_close(pdc)) {
+    error(2, "*** PDC_close failed ***");
+    exit(-1);
+  }
+
+  return 0;
+}
