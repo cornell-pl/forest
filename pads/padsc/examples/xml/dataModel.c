3f343c96885a7153d0e48322e76df87168718c70
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2003-07-17 20:03:17 +0000

    modifications

--- pads/padsc/examples/xml/dataModel.c
+++ pads/padsc/examples/xml/dataModel.c
@@ -1,33 +1,42 @@
+/* Bob: we need vtable for each base type in parse descriptors:
+   PDC_errCode_t, PDC_loc_t, PDC_pos_t, ...? 
+   PDC_flags_t, PDCI_flag.  We also need a vtable for each
+   base type. */
+
+
+
 /* Questions:
  * Giving parse descriptors and field the same names means we
  * get collisions on queries like: give me all the b's that are
  * greater than one.
  */
 
-/* libpadsc-xml.h/c */
+/* libpadsc-galax.h/c */
 /* define unknown_vtable for unknown types */
 /* define foo_vtable for all base types foo*/
 
-#include "CAML/CStuff.h"  /* value type */
-#include <pads-galax.h>
-#include <caml/fail.h>
+/* header files can be found in /usr/common/lib/ocaml/caml */
+#include <caml/mlvalues.h>  /* value type */
+#include <caml/fail.h>      /* exception */
+#include <libpads-galax.h>  /* header file for this file */
 
-typedef PDC_error_t (* childrenVT) (PDC_node_rep_t * node);
-typedef PDC_error_t (* typeValueVT) (PDC_node_rep_t * node);
+typedef PDC_node_rep_t ** (* childrenVT) (PDC_node_rep_t * node); 
+typedef value (* typeValueVT) (PDC_node_rep_t * node); 
+typedef const char * (* stringValueVT) (PDC_node_rep_t *node);
+
+/* leave these for later */
 typedef size_t (* write2ioVT) (Sfio_t *fp, PDC_node_rep_t * node);
-typedef size_t (* write2bufVT) (PDC_byte *buf, size_t buf_len, PDC_node_rep_t * node, int *buf_full);
-/* need vtable for each base type in parse descriptors:
-   PDC_errCode_t, PDC_loc_t, PDC_pos_t, ...? 
-   PDC_flags_t, PDCI_flag*/
+typedef size_t (* write2bufVT) (PDC_byte *buf, size_t buf_len, 
+				PDC_node_rep_t * node, int *buf_full);
 
 typedef struct PDC_vtable_s {
   childrenVT      children;
   typedValueVT    typed_value;
-  write2ioVT      write2io; 
-  write2bufVT     write2buf; 
+  stringValueVT   string_value;
 } PDC_vtable_t;
 
 typedef struct PDC_node_rep_s PDC_node_rep_t;
+
 struct PDC_node_rep_s {
   PDC_vtable_t   *vt;
   PDC_t          *pdc;
@@ -37,93 +46,115 @@ struct PDC_node_rep_s {
   void           *rep;
   const char     *name;
 };
-
-struct PDC_base_rep_s {
-  
-}; 
+/* Generic vtables */
+/* need one vtable for each pd of each kind of structured type:
+   struct, union, array, enum, typedef */
+const PDC_vtable_t struct_pd_vtable = {struct_pd_childrenVT, 
+				       PDCI_error_typed_valueVT,
+                                       0}; 
 
 
 /* Generic functions */
+/* PDC_node_rep_t ** generic_children (PDC_node_rep_t * node); */
 void **generic_children (void * ocamln)
 {
   PDC_node_rep_t *n = (PDC_node_rep_t *) ocamln; 
-  if (!n) return { failwith("Bad node representation in generic_children"); }
-  return n->vt.children(n);
+#ifndef NDEBUG
+  if (!n) { failwith("INVALID_PARAM: n null in generic_children"); }
+  if (!n->vt) 
+    { failwith("INVALID_PARAM: n->vt null in generic_children"); }
+#endif
+  return (void **) n->vt.children(n);
 }
+
 void *generic_parent (void * ocamln)
 {
   PDC_node_rep_t *n = (PDC_node_rep_t *) ocamln; 
-  if (!n) return { failwith("Bad node representation in generic_parent"); }
-  return n->parent;
+#ifndef NDEBUG
+  if (!n) { failwith("INVALID_PARAM: n null in generic_parent"); }
+#endif
+  return (void *)n->parent;
 }
 /* Return value is: 
 
    1. atomicValue -- a Caml object or 
    2. a union of PADS base types, which is converted
    to a Caml object on the Caml side. */
-generic_typed_value (void * ocamln)
+value generic_typed_value (void * ocamln)
 {
   PDC_node_rep_t *n = (PDC_node_rep_t *) ocamln; 
-  if (!n) return { failwith("Bad node representation in generic_typed_value"); }
+#ifndef NDEBUG
+  if (!n) {failwith("INVALID_PARAM: n null in generic_typed_value"); }
+  if (!n->vt) 
+    {failwith("INVALID_PARAM: n->vt null in generic_typed_value");}
+#endif
   return n->vt.typed_value(n);
 }
 
+
+const char * generic_string_value(void *ocamln){
+  PDC_node_rep_t *n = (PDC_node_rep_t *) ocamln; 
+#ifndef NDEBUG
+  if (!n) { failwith("INVALID_PARAM: n null in generic_string_value");}
+#endif
+  return "Not yet implemented";
+}
+
 /* struct, union, array case */
-PDC_error_t PDCI_error_typed_valueVT(PDC_node_rep_t *node){
-  return PDC_ERR;
+value PDCI_error_typed_valueVT(PDC_node_rep_t *node){
+  failwith("NOT_A_VALUE: typed_value called on structured type.");
+  return 0;  /* will never get here*/
 } 
 
-ssize_t PDCI_null_write2ioVT(PDC_node_rep_t *node){
-  return 0;  /* eventually generate type specific parse descriptor calls */
-} 
-
-ssize_t PDCI_null_write2bufVT(PDC_node_rep_t *node){
-  return 0;   /* eventually generate type specific parse descriptor calls */
-} 
-
-/* in generated .h file for foo pads declaration*/
-PDC_error_t foo_children(PDC_node_rep_t *node);
-PDC_error_t foo_typed_value(PDC_node_rep_t *node); 
-size_t foo_write2ioVT(Sfio_t *fp, PDC_node_rep_t *node);
-size_t foo_write2bufVT (PDC_byte *buf, size_t buf_len, PDC_node_rep_t * node, int *buf_full);
+/*****************************************************************/
+/* in generated p1-galax.h file for foo pads declaration*/
+PDC_node_rep_t ** foo_children(PDC_node_rep_t *node);
+/*foo_typed_value: always uses error version in library for structured types*/
+/*foo_string_value: not yet implemented. */
+/* do we need a function to create nodes? 
+   PDC_node_rep_t * foo_mk_node(PDC_t *pdc, PDC_node)_ */
 
 extern const PDC_vtable_t foo_vtable;
-extern const PDC_vtable_t foo_pd_vtable;
 
-/* in generated .c file for foo pads declaration */
+/*****************************************************************/
+/* in generated p1-galax.c file for foo pads declaration */
 /* struct case */
-const PDC_vtable_t foo_vtable = {foo_childrenVT, PDCI_error__typed_valueVT, 
-				 foo_write2ioVT, foo_write2bufVT};
-const PDC_vtable_t foo_pd_vtable = {foo_pd_childrenVT, PDCI_error__typed_valueVT, 
-				    PDCI_null_write2ioVT, PDCI_null_write2bufVT};
+#include <foo-galax.h>
+/* assume foo is a struct */
+const PDC_vtable_t foo_vtable = {foo_children, PDCI_error_typed_value, 0};
 
-
-PDC_error_t foo_childrenVT(PDC_node_rep_t *parent){
-  foo *rep = (foo *) parent->rep;
-  foo_pd *pd = (foo_pd *) parent->pd;
-  foo_m *m = (foo_m *) parent->m;
+PDC_node_rep_t** foo_children(PDC_node_rep_t *self){
+  foo *rep = (foo *) self->rep;
+  foo_pd *pd = (foo_pd *) self->pd;
+  foo_m *m = (foo_m *) self->m;
   PDC_node_rep_t *current;
-  parent->numChildren =<Compiler-supplied constant: 
-                        number of full fields + number of computed fields + 1>;
-  if (!(parent->children = vmnewof(parent->pdc->vm, 0, PDC_node_rep_t, parent->numChildren, 0))){
-    return PDC_ERR;
+  int numChildren = <Compiler-supplied constant: 
+                     number of full fields + number of computed fields + 1>;
+  PDC_node_rep_t** result; 
+  /* vmnewof call must be changed so we can call it*/
+  if (!(result = (PDC_node_rep_t**)PDCI_newof(self->pdc->vm, 0, 
+					      sizeof(PDC_node_rep_t*), 
+					      numChildren, 0))){
+    failwith("ALLOC_ERROR: in foo_children");
   };
   /* set children m, pd, and rep */
   /* parse descriptor child */
-  current = &(parent->children[0]);
+  result[0] = (PDC_node_rep_t *)PDCI_newof(self->pdc->vm, 0, 
+					   sizeof(PDC_node_rep_t), 1, 0);
+  current = result[0];
   current->vt = foo_pd_vtable;
-  current->pdc = parent->pdc;
-  current->parent = parent;
+  current->pdc = self->pdc;
+  current->self = self;
   current->m = (void *)0;
   current->pd = (void *)0;
   current->rep = (void *)pd;
   current->name = "parseDesc";
   
   /* now do normal fields: assume first field is bar b */
-  current = &(parent->children[1]);
+  current = &(self->children[1]);
   current->vt = bar_vtable;
-  current->pdc = parent->pdc;
-  current->parent = parent;
+  current->pdc = self->pdc;
+  current->self = self;
   current->m = (void *)&(m->b);
   current->pd = (void *)&(pd->b);
   current->rep = (void *)&(rep->b);
