3624474bf31049804661207ec3c7d6a647ce1c92
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2003-07-22 13:42:14 +0000

    fixed example code gen: node_rep should be node, .c file needs to include camls/fail.h, .h file needs to include the normal generated .h file p1.h

--- pads/padsc/examples/xml/p1-galax.c
+++ pads/padsc/examples/xml/p1-galax.c
@@ -1,6 +1,9 @@
 /*****************************************************************/
 /* in generated p1-galax.c file from pads file p1.p */
+
 #include "p1-galax.h"
+#include "caml/fail.h"      /* exception */
+
 /* struct case */
 /* assume fooStruct is a struct in p1.p*/
 
@@ -8,12 +11,12 @@
    (number of full fields + number of computed fields + 1) 
    for the parse descriptor.*/
 
-PDCI_node_rep_t** fooStruct_children(PDCI_node_rep_t *self){
+PDCI_node_t** fooStruct_children(PDCI_node_t *self){
   fooStruct    *rep = (fooStruct *)    self->rep;
   fooStruct_pd *pd  = (fooStruct_pd *) self->pd;
   fooStruct_m  *m   = (fooStruct_m *)  self->m;
-  PDCI_node_rep_t** result; 
-  if (!(result = PDCI_NEW_NODE_PTR_LIST(self->pdc, <numChildren>))) {
+  PDCI_node_t** result; 
+  if (!(result = PDCI_NEW_NODE_PTR_LIST(self->pdc, /* <numChildren> */ 2 ))) {
     failwith("ALLOC_ERROR: in fooStruct_children");
   };
   /* parse descriptor child */
@@ -31,18 +34,18 @@ const PDCI_vtable_t fooStruct_vtable = {fooStruct_children,
 
 
 /* ENUM CASE fooEnum */
-PDCI_node_rep_t** fooEnum_children(PDCI_node_rep_t *self){
-  const char  *Cstr;
+PDCI_node_t** fooEnum_children(PDCI_node_t *self){
+  char  *Cstr;
   fooEnum     *rep  = (fooEnum *)     self->rep;
   PDC_base_pd *pd   = (PDC_base_pd *) self->pd;
-  PDCI_node_rep_t** result;
+  PDCI_node_t** result;
   if (!(result = PDCI_NEW_NODE_PTR_LIST(self->pdc, 2))) {
     failwith("ALLOC_ERROR: in fooEnum_children");
   };
   /* parse descriptor child */
-  PDCI_MK_TNODE(result[0], &PDCI_base_pd_vtable, self, "pd", pd, "fooEnum_children");
+  PDCI_MK_TNODE(result[0], &PDC_base_pd_vtable, self, "pd", pd, "fooEnum_children");
   /* string val child */
-  Cstr = fooEnum2str(*rep);
+  Cstr = (char*)fooEnum2str(*rep);
   PDCI_MK_TNODE(result[0], &PDCI_Cstr_val_vtable, self, "val", Cstr, "fooEnum_children");
   return result;
 }
@@ -52,11 +55,11 @@ const PDCI_vtable_t fooEnum_vtable = {fooEnum_children,
 
 
 /* TYPEDEF CASE, typedef fooBase fooTy*/
-PDCI_node_rep_t** fooTy_children(PDCI_node_rep_t *self){
+PDCI_node_t** fooTy_children(PDCI_node_t *self){
   fooTy    *rep = (fooTy *)    self->rep;
   fooTy_pd *pd  = (fooTy_pd *) self->pd;
   fooTy_m  *m   = (fooTy_m *)  self->m;
-  PDCI_node_rep_t** result; 
+  PDCI_node_t** result; 
   if (!(result = PDCI_NEW_NODE_PTR_LIST(self->pdc, 2))) {
     failwith("ALLOC_ERROR: in fooTy_children");
   };
@@ -73,11 +76,11 @@ const PDCI_vtable_t fooTy_vtable = {fooTy_children,
 				    PDCI_error_typed_value, 0}; 
 
 /* UNION CASE fooUnion */
-PDCI_node_rep_t** fooUnion_children(PDCI_node_rep_t *self){
+PDCI_node_t** fooUnion_children(PDCI_node_t *self){
   fooUnion    *rep = (fooUnion *)    self->rep;
   fooUnion_pd *pd  = (fooUnion_pd *) self->pd;
   fooUnion_m  *m   = (fooUnion_m *)  self->m;
-  PDCI_node_rep_t** result; 
+  PDCI_node_t** result; 
 
   const char *branch = fooUnion_tag2str(rep->tag);
 
@@ -92,6 +95,10 @@ PDCI_node_rep_t** fooUnion_children(PDCI_node_rep_t *self){
   /* handle branches: assume first branch is tagty1 tag1 */
     PDCI_MK_NODE(result[1], &tagty1_vtable,self,branch,
 		 &(m->tag1),&(pd->val.tag1),&(rep->val.tag1), "fooUnion_children");
+    break;
+
+  case tag2: 
+    /* as above */
     break;
 
   /* ... repeat for all other branches ... */
@@ -109,11 +116,12 @@ const PDCI_vtable_t fooUnion_vtable = {fooUnion_children,
 
 /* ARRAY CASE fooArray with element type fooElement */
 
-PDCI_node_rep_t** fooArray_children(PDCI_node_rep_t *self){
+PDCI_node_t** fooArray_children(PDCI_node_t *self){
   fooArray    *rep = (fooArray *)    self->rep;
   fooArray_pd *pd  = (fooArray_pd *) self->pd;
   fooArray_m  *m   = (fooArray_m *)  self->m;
-  PDCI_node_rep_t** result; 
+  PDCI_node_t** result; 
+  int i;
   if (!(result = PDCI_NEW_NODE_PTR_LIST(self->pdc, rep->length + 2))) {
     failwith("ALLOC_ERROR: in fooArray_children");
   };
--- pads/padsc/examples/xml/p1-galax.h
+++ pads/padsc/examples/xml/p1-galax.h
@@ -1,28 +1,46 @@
+#include "p1.h"
 #include "pglx-internal.h"
+
 
 /*****************************************************************/
 /* in generated p1-galax.h file from pads file p1.p */
 /* typed_value: always uses error version in library for structured types*/
 /* string_value: not yet implemented. */
 
+/* there is some type named bar in p1.p */
+PDCI_node_t ** bar_children(PDCI_node_t *node);
+extern const PDCI_vtable_t bar_vtable;
+
+/* there is some type named fooBase in p1.p */
+PDCI_node_t ** fooBase_children(PDCI_node_t *node);
+extern const PDCI_vtable_t fooBase_vtable;
+
+/* there is some type named fooElement in p1.p */
+PDCI_node_t ** fooElement_children(PDCI_node_t *node);
+extern const PDCI_vtable_t fooElement_vtable;
+
+/* there is some type named tagty1 in p1.p */
+PDCI_node_t ** tagty1_children(PDCI_node_t *node);
+extern const PDCI_vtable_t tagty1_vtable;
+
 /* assume fooStruct is a struct in p1.p*/
-PDCI_node_rep_t ** fooStruct_children(PDCI_node_rep_t *node);
+PDCI_node_t ** fooStruct_children(PDCI_node_t *node);
 extern const PDCI_vtable_t fooStruct_vtable;
 
 /* assume fooEnum is an enum in p1.p*/
-PDCI_node_rep_t ** fooEnum_children(PDCI_node_rep_t *node);
+PDCI_node_t ** fooEnum_children(PDCI_node_t *node);
 extern const PDCI_vtable_t fooEnum_vtable;
 
 /* assume fooTy is a typedef in p1.p*/
-PDCI_node_rep_t ** fooTy_children(PDCI_node_rep_t *self);
+PDCI_node_t ** fooTy_children(PDCI_node_t *self);
 extern const PDCI_vtable_t fooTy_vtable; 
 
 /* assume fooUnion is a union in p1.p*/
-PDCI_node_rep_t** fooUnion_children(PDCI_node_rep_t *self);
+PDCI_node_t** fooUnion_children(PDCI_node_t *self);
 extern const PDCI_vtable_t fooUnion_vtable; 
 
 /* assume fooArray is an array in p1.p*/
-PDCI_node_rep_t** fooArray_children(PDCI_node_rep_t *self);
+PDCI_node_t** fooArray_children(PDCI_node_t *self);
 extern const PDCI_vtable_t fooArray_vtable;
 
 
