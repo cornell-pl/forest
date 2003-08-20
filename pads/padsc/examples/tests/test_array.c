5b2a454ce8262be216cde77930752c28b943e317
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2003-08-20 17:50:51 +0000

    test_array was not using latest APIs

--- pads/padsc/examples/tests/test_array.c
+++ pads/padsc/examples/tests/test_array.c
@@ -2,40 +2,41 @@
 #include "array.h"
 
 int main(int argc, char** argv) {
-  PDC_t*           pdc;
-  intList            f1data = {0,0,0};
+  PDC_t*             pdc;
+  intList            rep;
+  intList_m          m;
+  intList_pd         pd;
   intList_acc        accum;
-  intList_pd         pd = {0};
+
+
 
   if (PDC_ERR == PDC_open(&pdc,0,0)) {
-    error(2, "*** PDC_open failed ***");
-    exit(-1);
+    error(ERROR_FATAL, "*** PDC_open failed ***");
   }
   if (PDC_ERR == PDC_IO_fopen(pdc, "../../data/array")) {
-    error(2, "*** PDC_IO_fopen failed ***");
-    exit(-1);
+    error(ERROR_FATAL, "*** PDC_IO_fopen failed ***");
   }
 
-  error(0, "\ninit the accum");
-  if (PDC_ERR == intList_acc_init(pdc, &accum)) {
-    error(2, "** init failed **");
-    exit(-1);
-  }
+  error(0, "\ninit everything");
+  intList_init(pdc, &rep);
+  intList_m_init(pdc, &m, PDC_CheckAndSet);
+  intList_pd_init(pdc, &pd);
+  intList_acc_init(pdc, &accum);
 
   /*
    * Try to read each line of data
    */
   while (!PDC_IO_at_EOF(pdc)) {
     error(0, "\ncalling intList_read");
-    if (PDC_OK == intList_read(pdc, 0, &pd, &f1data)) {
+    if (PDC_OK == intList_read(pdc, &m, &pd, &rep)) {
       /* do something with the data */
-      error(2, "intList_read returned array of length: %d", f1data.length);
-      if (PDC_ERR == intList_acc_add(pdc, &accum, &pd, &f1data)) {
+      error(2, "intList_read returned array of length: %d", rep.length);
+      if (PDC_ERR == intList_acc_add(pdc, &accum, &pd, &rep)) {
 	error(0, "** accum_add failed **");
       }
     } else {
       error(2, "intList_read returned: error");
-      if (PDC_ERR == intList_acc_add(pdc, &accum, &pd, &f1data)) {
+      if (PDC_ERR == intList_acc_add(pdc, &accum, &pd, &rep)) {
 	error(0, "** accum_add failed **");
       }
     }
@@ -47,13 +48,11 @@ int main(int argc, char** argv) {
   }
 
   if (PDC_ERR == PDC_IO_close(pdc)) {
-    error(2, "*** PDC_IO_close failed ***");
-    exit(-1);
+    error(ERROR_FATAL, "*** PDC_IO_close failed ***");
   }
 
   if (PDC_ERR == PDC_close(pdc)) {
-    error(2, "*** PDC_close failed ***");
-    exit(-1);
+    error(ERROR_FATAL, "*** PDC_close failed ***");
   }
 
   return 0;
