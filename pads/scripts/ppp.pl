1786fc4d8e539f1511e80e010d0c0ddc29ea9496
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2003-10-08 16:02:16 +0000

    added fw int case

--- pads/scripts/ppp.pl
+++ pads/scripts/ppp.pl
@@ -4,75 +4,75 @@ my $tfmt = "%-30s";
 my %pads_ty =
   (
    d => "Pint",
-   u => "Puint"
+   u => "Puint",
+   s => "Pstring"
   );
 my %stop_re =
   (
-   d => "/[+-]?\d/",
-   u => "/[+]?\d/"
+   d => "/[+-]?\\d/",
+   u => "/[+]?\\d/",
+   s => undef
   );
 
-my ($has_re, $elt, $indent, $litpre, $toparse, $expr, $before, $after, $d, $ty, $pty, $parsed, $follow_re);
-my $open_chars = "{/";
+my ($has_re, $elt, $indent, $litpre, $toparse, $expr, $after, $d, $w, $ty, $pty, $parsed, $follow_re);
 
 my $line = 0;
 LINE: while (<>) {
   $line++;
-  if (/^(\s*){(\s*Pre\s*)?:(.*):}(;)?$/) {
+  if (/^(\s*){(\s*Pre\s*)?:(.+):}(;)?$/) {
+    # found a {: ... :} block
+
     ($indent, $has_re, $toparse) = ($1, $2, $3);
     $has_re = "Pre " if ($has_re);
     $litpre = "$indent$has_re";
     $parsed = "";
     $follow_re = "/\$/";
-    EXPR: while (1) {
+    
+    while (1) {
       if (!$toparse) {
+	# done with entire {: ... :} block
 	print $parsed;
 	next LINE;
       }
       # look at %<expr> forms one at a time, right to left 
       if ($toparse =~ /^(.*[^\\]%|%)(.*?)$/) {
-	($before, $expr) = ($1, $2);
-	$before =~ s/%$//;
-	$toparse = $before;
-	if ($expr =~ /^I(\d+)([du])[(](\w+)[)](.*?)$/) {
-	  ($d, $ty, $id, $after) = ($1, $2, $3, $4);
+	($toparse, $expr) = ($1, $2);
+	$toparse =~ s/%$//;
+	if ($expr =~ /^(\d*)I(\d+)([du])[(](\w+)[)](.*?)$/) {
+	  ($w, $d, $ty, $id, $after) = ($1, $2, $3, $4, $5);
 	  my $i = 8;
 	  if ($d == 64) {
 	    $i = 64;
 	  } else {
 	    while ($i < ($d * 8)) { $i++; }
 	  }
+	  $pty = "$pads_ty{$ty}$i";
+	  $pty .= "_FW(:${w}:)" if ($w);
+	} elsif ($expr =~ /^(\d*)(s)[(](\w+)[)](.*?)$/) {
+	  ($w, $ty, $id, $after) = ($1, $2, $3, $4);
 	  $pty = $pads_ty{$ty};
-	  $elt = sprintf("$indent$tfmt %s;\n", "$pty$i", $id);
-	  $elt .= "$litpre\"$after\";\n" if ($after);
-	  $parsed = "$elt$parsed";
-	  $follow_re = $stop_re{$ty};
-	  next EXPR;
-	}
-	if ($expr =~ /^(\d+)s[(](\w+)[)](.*?)$/) {
-	  ($d, $id, $after) = ($1, $2, $3);
-	  $elt = sprintf("$indent$tfmt %s;\n", "Pstring_FW(:$d:)", $id);
-	  $elt .= "$litpre\"$after\";\n" if ($after);
-	  $parsed = "$elt$parsed";
-	  $follow_re = undef;
-	  next EXPR;
-	}
-	if ($expr =~ /^s[(](\w+)[)](.*?)$/) {
-	  ($id, $after) = ($1, $2);
-	  if ($after) {
-	    my $first_follow = substr($after, 0, 1);
-	    $follow_re = "/[$first_follow]/";
+	  if ($w) {
+	    $pty .= "_FW(:${w}:)";
+	  } else {
+	    # need a stop re
+	    if ($after) {
+	      my $first_follow = substr($after, 0, 1);
+	      # XXX need to deal with case where $first_follow is a special char XXX
+	      $follow_re = "/[$first_follow]/";
+	    }
+	    die "Cannot automatically determine what follows the %s($id) on line $line\n"
+	      if (!defined($follow_re));
+	    $pty .= "_SE(:\"$follow_re\":)";
 	  }
-	  die "Cannot automatically determine what follows the %s($id) on line $line\n"
-	    if (!defined($follow_re));
-	  $elt = sprintf("$indent$tfmt %s;\n", "Pstring_SE(:\"$follow_re\":)", $id);
-	  $elt .= "$litpre\"$after\";\n" if ($after);
-	  $parsed = "$elt$parsed";
-	  $follow_re = undef;
-	  next EXPR;
+	} else {
+	  # Did not understand this %<expr>
+	  die "Did not understand this %<expr> expression: %$expr on line $line\n";
 	}
-	# Did not understand this %<expr>
-	die "Did not understand this %<expr> expression: %$expr on line $line\n";
+	# determined pty, id, after
+	$elt = sprintf("$indent$tfmt %s;\n", $pty, $id);
+	$elt .= "$litpre\"$after\";\n" if ($after);
+	$parsed = "$elt$parsed";
+	$follow_re = $stop_re{$ty};
       } else {
 	# last case is to match a leading non %<expr>
 	$parsed = "$litpre\"$toparse\";\n$parsed";
--- pads/scripts/test.p
+++ pads/scripts/test.p
@@ -1,5 +1,5 @@
 Pstruct s1 {
-    {:|%I8d(an_int64)|%I4u(a_uint32)|foo:}
+    {:|%I8d(an_int64)|%I4u(a_uint32)|%7I4u(a_uint32_fw7)foo:}
     {:%8s(a_fw8_string)%s(a_vbar_term_string)|%s(an_eor_term_string):}
     {Pre:\s+%s(an_int_term_string)%I4d(an_int32)\t:}
 };
