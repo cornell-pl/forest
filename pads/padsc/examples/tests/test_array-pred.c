d1a7da06c7cfb3b92e5ad1a61e9d18bd46b6d5cc
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2003-10-09 18:06:17 +0000

    added new example program

---
+++ pads/padsc/examples/tests/test_array-pred.c
@@ -0,0 +1,59 @@
+#include "pads.h"
+#include "array-pred.h"
+
+int main(int argc, char** argv) {
+  P_t*             pdc;
+  entry            rep;
+  entry_m          m;
+  entry_pd         pd;
+  entry_acc        accum;
+
+
+
+  if (P_ERR == P_open(&pdc,0,0)) {
+    error(ERROR_FATAL, "*** P_open failed ***");
+  }
+  if (P_ERR == P_io_fopen(pdc, "../../data/array-pred")) {
+    error(ERROR_FATAL, "*** P_io_fopen failed ***");
+  }
+
+  error(0, "\ninit everything");
+  entry_init(pdc, &rep);
+  entry_m_init(pdc, &m, P_CheckAndSet);
+  entry_pd_init(pdc, &pd);
+  entry_acc_init(pdc, &accum);
+
+  /*
+   * Try to read each line of data
+   */
+  while (!P_io_at_eof(pdc)) {
+    error(0, "\ncalling entry_read");
+    if (P_OK == entry_read(pdc, &m, &pd, &rep)) {
+      /* do something with the data */
+      error(2, "entry_read returned array of length: %d", rep.seq.length);
+      if (P_ERR == entry_acc_add(pdc, &accum, &pd, &rep)) {
+	error(0, "** accum_add failed **");
+      }
+    } else {
+      error(2, "entry_read returned: error");
+      if (P_ERR == entry_acc_add(pdc, &accum, &pd, &rep)) {
+	error(0, "** accum_add failed **");
+      }
+    }
+  }
+  error(0, "\nFound eof");
+  error(0, "\nDescribe the accum");
+  if (P_ERR == entry_acc_report(pdc, "top", 0, 0, &accum)) {
+    error(0, "** accum_report failed **");
+  }
+
+  if (P_ERR == P_io_close(pdc)) {
+    error(ERROR_FATAL, "*** P_io_close failed ***");
+  }
+
+  if (P_ERR == P_close(pdc)) {
+    error(ERROR_FATAL, "*** P_close failed ***");
+  }
+
+  return 0;
+}
