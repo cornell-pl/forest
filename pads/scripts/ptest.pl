1448ed39ad1df89f01b2bedcee146c0b7fc19f46
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2004-03-03 18:45:26 +0000

    added --debug and --maxrecs options

--- pads/scripts/ptest.pl
+++ pads/scripts/ptest.pl
@@ -6,7 +6,27 @@ if ($pads_home eq "") {
   print "\n    set PADS_HOME and try again\n\n";
   exit(-1);
 }
-goto usage if ($#ARGV < 1 || $#ARGV > 2);
+my $maxrecs = 0;
+my $readmask = "P_CheckAndSet";
+my $parsing_opt_args = 1;
+ARG: while ($parsing_opt_args) {
+  if ($#ARGV > 0 && ($ARGV[0] eq "-m" || $ARGV[0] eq "--maxrecs")) {
+    shift(@ARGV);
+    $maxrecs = shift(@ARGV);
+    next ARG;
+  }
+  if ($#ARGV >= 0 && ($ARGV[0] eq "-d" || $ARGV[0] eq "--debug")) {
+    shift(@ARGV);
+    $readmask = "P_CheckAndSet|P_DbgRead";
+    next ARG;
+  }
+  if ($ARGV[0] =~ /^[-]/) {
+    print "\n    Unknown option: $ARGV[0]\n";
+    goto usage;
+  }
+  $parsing_opt_args = 0;
+}
+goto usage if ($#ARGV != 1);
 my $package = "$pads_home/ast-ast/bin/package.cvs";
 my $arch = `$package` || die "\n    Could not execute $package\n\n";
 chomp($arch);
@@ -36,28 +56,28 @@ $pname =~ s/[.]p$//;
 my $pspec_c = "$pname.c";
 my $pspec_h = "$pname.h";
 my $pty = $ARGV[1];
-my $maxrecs = ($#ARGV == 2) ? $ARGV[2] : 0;
 my $mfile = "tmp_GNUmakefile.ptest";
 my $efile = "tmp_rwxml_$pname";
+my $efile_d = $efile . "_d";
 my $cfile = "$efile.c";
 my $gendir = ".";
-# XXX_TODO if exists ./gen or ../gen then set gendir to that directory
 
-print "XXX_REMOVE pspec = $pspec\n";
-print "XXX_REMOVE ppath = $ppath\n";
-print "XXX_REMOVE pname = $pname\n";
-print "XXX_REMOVE pspec_c = $pspec_c\n";
-print "XXX_REMOVE pspec_h = $pspec_h\n";
-print "XXX_REMOVE pty = $pty\n";
-print "XXX_REMOVE maxrecs = $macrecs\n";
-print "XXX_REMOVE mfile = $mfile\n";
-print "XXX_REMOVE efile = $efile\n";
-print "XXX_REMOVE cfile = $cfile\n";
-print "\n";
+# print "XXX_REMOVE pspec = $pspec\n";
+# print "XXX_REMOVE ppath = $ppath\n";
+# print "XXX_REMOVE pname = $pname\n";
+# print "XXX_REMOVE pspec_c = $pspec_c\n";
+# print "XXX_REMOVE pspec_h = $pspec_h\n";
+# print "XXX_REMOVE pty = $pty\n";
+# print "XXX_REMOVE maxrecs = $macrecs\n";
+# print "XXX_REMOVE mfile = $mfile\n";
+# print "XXX_REMOVE efile = $efile\n";
+# print "XXX_REMOVE efile_d = $efile_d\n";
+# print "XXX_REMOVE cfile = $cfile\n";
+# print "\n";
 
 $mtmpl =~ s/=GENDIR=/$gendir/g;
 $mtmpl =~ s/=PPATH=/$ppath/g;
-$mtmpl =~ s/=EFILE=/$efile/g;
+$mtmpl =~ s/=EFILE=/$efile_d/g;
 
 
 open (MOUT, ">$mfile") || die "\n    Could not open $mfile for output\n\n";
@@ -67,6 +87,7 @@ close (MOUT);
 open (COUT, ">$cfile") || die "\n    Could not open $cfile for output\n\n";
 print COUT "#define PADS_TY(suf) $pty ## suf
 #define MAX_RECS $maxrecs
+#define READ_MASK $readmask
 #define IO_DISC_MK P_nlrec_make(0)
 #include \"$pspec_h\"
 #include \"template/read_orig_write_xml.h\"
@@ -84,8 +105,8 @@ if ($res =~ /error/is) {
   print "\n$res\n\nBuild of test program failed, see error(s) above\n\n";
   exit(-1);
 }
-print "\nRunning test program.  Input is stdin, you must hit ^D to terminate input and get results.\n\n";
-my $res2 = `$arch/$efile 2>&1`;
+print "\nRunning test program.\nInput is stdin, you must hit ^D to terminate input and get results.\n\n";
+my $res2 = `$arch/$efile_d 2>&1`;
 $res2 =~ s|Input file = /dev/stdin||s;
 $res2 =~ s|Output file = /dev/stdout||s;
 while ($res2 =~ m|\n\n|) {
@@ -95,6 +116,5 @@ print "\nResult:\n$res2\n";
 exit(0);
 
 usage:
-print "\n    usage:  ptest.pl <pspec> <ptype> [ <maxrecs> ]\n\n";
+print "\n    usage:  ptest.pl [ -m/--maxrecs # ] [ -d/--debug ] <pspec> <ptype>\n\n";
 exit(-1);
-
