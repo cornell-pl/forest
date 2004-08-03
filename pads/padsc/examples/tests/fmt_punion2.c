df700fa9e884abb2889afda498b369790a4a8574
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2004-08-03 13:40:32 +0000

    test files for fmt functions

---
+++ pads/padsc/examples/tests/fmt_punion2.c
@@ -0,0 +1,17 @@
+#define DEF_INPUT_FILE "../../data/punion2"
+
+#define EXTRA_GOOD_READ_CODE do { \
+  error(2, "test2_read returned: id %s", test2_tag2str(rep.tag)); \
+} while (0)
+
+#define EXTRA_BAD_READ_CODE do { \
+  error(2, "test2_read returned: error"); \
+} while (0)
+
+#define PADS_TY(suf) test2 ## suf
+#define PPADS_TY(pref) pref ## test2
+#define DELIMS ":,"
+#define FMT_MASK P_Write | P_WriteMeta
+#include "punion2.h"
+#include "template/read_format.h"
+
---
+++ pads/padsc/examples/tests/fmt_struct4.c
@@ -0,0 +1,16 @@
+#define DEF_INPUT_FILE "../../data/struct"
+
+#define EXTRA_GOOD_READ_CODE do { \
+  error(2, "testtwo_read returned: id %d  ts %d  f %d ", rep.header.id, rep.header.ts, rep.f); \
+} while (0)
+
+#define EXTRA_BAD_READ_CODE do { \
+  error(2, "testtwo_read returned: error"); \
+} while (0)
+
+#define PADS_TY(suf) testtwo ## suf
+#define PPADS_TY(pref) pref ## testtwo
+#define DELIMS ","
+#include "struct4.h"
+#include "template/read_format.h"
+
