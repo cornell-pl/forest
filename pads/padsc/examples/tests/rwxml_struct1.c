55c946d8b710f85bbd79e81c04b65391e04aaadc
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2004-10-17 04:24:53 +0000

    had to rename struct to struct1, system file called struct.h exists on gs1

--- pads/padsc/examples/tests/rwxml_struct1.c
+++ pads/padsc/examples/tests/rwxml_struct1.c
@@ -1,4 +1,4 @@
-#define DEF_INPUT_FILE "../../data/struct"
+#define DEF_INPUT_FILE "../../data/struct1"
 #define PADS_TY(suf) testtwo ## suf
 #include "struct.h"
 #include "template/read_orig_write_xml.h"
