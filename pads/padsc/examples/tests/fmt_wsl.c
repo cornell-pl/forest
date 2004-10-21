cdb62cad36a6d7316508c6d3be76b97bf06594c6
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2004-10-21 12:16:42 +0000

    format program for wsl

---
+++ pads/padsc/examples/tests/fmt_wsl.c
@@ -0,0 +1,67 @@
+#include "wsl.h"
+
+#define DEF_INPUT_FILE "../../data/ai.big"
+
+// fmt function used to override the default Pip fmt function
+ssize_t
+my_Pip_fmt2buf(P_t *pads, Pbyte *buf, size_t buf_len,
+	       int *buf_full, int *requested_out, char const *delims,
+	       void *m, void  *pd, void *rep, va_list type_args)
+{
+  Pip     *t_rep = (Pip*)    rep;
+  Sfio_t     *tmpstr;
+  ssize_t     res;
+
+  (*requested_out) = 1;
+  if (!(tmpstr = sfstropen ())) { 
+    return -1;
+  }
+  res = sfprintf(tmpstr, "%d.%d.%d.%d", t_rep->elts[0], t_rep->elts[1],
+		 t_rep->elts[2],t_rep->elts[3]);
+  if (res > buf_len) { // not enough space in buffer
+    (*buf_full) = 1;
+    return -1;
+  }
+  memcpy(buf, sfstruse(tmpstr), res);
+  return res;
+}
+
+// fmt function used to override the default Phostname fmt function
+ssize_t
+my_Phostname_fmt2buf(P_t *pads, Pbyte *buf, size_t buf_len,
+	       int *buf_full, int *requested_out, char const *delims,
+	       void *m, void  *pd, void *rep, va_list type_args)
+{
+  Phostname  *t_rep = (Phostname*)    rep;
+  Sfio_t     *tmpstr;
+  ssize_t     res;
+
+  (*requested_out) = 1;
+  if (!(tmpstr = sfstropen ())) { 
+    return -1;
+  }
+  res = sfprintf(tmpstr, "%s.%s.%s.%s", t_rep->elts[0].str, t_rep->elts[1].str,
+		 t_rep->elts[2].str,t_rep->elts[3].str);
+  if (res > buf_len) { // not enough space in buffer
+    (*buf_full) = 1;
+    return -1;
+  }
+  memcpy(buf, sfstruse(tmpstr), res);
+  return res;
+}
+
+//#define MASK_MOD   P_Dont_Write(m.compoundLevel);
+//#define MASK_MOD   P_Dont_Write(m.header.c); P_Do_WriteVoid(m.header.c); P_Dont_Write(m.header.ts); P_Do_WriteVoid(m.header.ts);
+//#define MASK_MOD   P_Dont_Write(m.header.compoundLevel);
+
+#define FMT_OVERRIDE_TY1   "Pip"
+#define FMT_OVERRIDE_FN1   my_Pip_fmt2buf
+#define FMT_OVERRIDE_TY2   "Phostname"
+#define FMT_OVERRIDE_FN2   my_Phostname_fmt2buf
+
+//#define DATE_OUT_FMT "%K"
+
+#define DATE_OUT_FMT "%m%d%Y:%T"
+#define PADS_TY(suf) entry_t ## suf
+#define DELIMS "|"
+#include "template/read_format.h"
