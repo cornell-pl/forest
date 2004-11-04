409c88fbc9a32bb6a9ac852419ea8574532786e7
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2004-11-04 22:25:33 +0000

    added baseline record reading program

--- pads/padsc/examples/tests/dibbler_select.c
+++ pads/padsc/examples/tests/dibbler_select.c
@@ -75,6 +75,7 @@ int main(int argc, char** argv) {
   // turn off setting for entire header
   order_header_t_m_init(pads, &(entry_m.header), P_Set);
   zip_code_t_m_init(pads, &entry_m.header.zip_code, P_CheckAndSet);  // Necessary for proper parsing of union
+  entry_m.header.zip_code.compoundLevel = P_Ignore;
   // turn back on setting of header's order_num field
   entry_m.header.order_num = P_Set;
 
