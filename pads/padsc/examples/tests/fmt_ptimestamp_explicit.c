e7b1cc467f178bcf234419d1c411a5b30a21b603
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2005-02-15 22:23:35 +0000

    more tests

--- pads/padsc/examples/tests/fmt_ptimestamp_explicit.c
+++ pads/padsc/examples/tests/fmt_ptimestamp_explicit.c
@@ -1,5 +1,7 @@
 #define DEF_INPUT_FILE "../../data/ptimestamp_explicit"
 
+#define TIMESTAMP_EXPLICIT_OUT_FMT "default_timestamp_explicit:%K"
+
 #define WSPACE_OK 1
 #define PADS_TY(suf) threetimestamps ## suf
 #define DELIMS "|,"
