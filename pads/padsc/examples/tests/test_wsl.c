89739a5be9b2e5ba9b52f44171c009fb811ab0ec
joel <joel@5d86edc2-cc7b-4035-901b-baf054620955>
2005-02-15 14:14:09 +0000

    Updating for Kathleen

--- pads/padsc/examples/tests/test_wsl.c
+++ pads/padsc/examples/tests/test_wsl.c
@@ -1,7 +1,4 @@
-/*@FILE @LEFT wsl_accum.tex */
-/*@BEGIN wsl_accum.tex */
-#define DEF_INPUT_FILE "../../data/ai"
 #define PADS_TY(suf) entry_t ## suf
+#define IO_DISC_MK P_nlrec_make(0)
 #include "wsl.h"
 #include "template/accum_report.h"
-/*@END wsl_accum.tex */
