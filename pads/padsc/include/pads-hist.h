e67c30a09a24268a1d466a867055180f458961fe
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2005-05-23 01:09:35 +0000

    added files for histograms

---
+++ pads/padsc/include/pads-hist.h
@@ -0,0 +1,15 @@
+/*
+ * PADS histogram header file
+ * 
+ */
+
+#ifndef __PADS_H__
+#error "Do not include pads_hist.h directly -- include pads.h instead"
+#endif
+
+#ifndef __P_HIST_H__
+#define __P_HIST_H__
+
+// Put prototypes for histogram functions here.
+
+#endif /*  __P_HIST_H__  */
---
+++ pads/padsc/libpads/pads-hist.c
@@ -0,0 +1,10 @@
+/*
+ * histogram implementation
+ * 
+ */
+
+#include "pads-internal.h"
+
+// Code to implement histograms, both generic and per type
+
+
