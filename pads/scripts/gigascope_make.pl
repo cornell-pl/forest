77c93bd69d535413dce7a8ed34ba011cdf2fb46f
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2005-05-29 21:02:11 +0000

    forgot to restore removal of temp files

--- pads/scripts/gigascope_make.pl
+++ pads/scripts/gigascope_make.pl
@@ -396,7 +396,7 @@ print TRF $ttext;
 close (TRF);
 print "\nDone creating $make_file\n" if ($dbg);
 
-#$res = `/bin/rm -f $temp_files > /dev/null 2>&1`;
+$res = `/bin/rm -f $temp_files > /dev/null 2>&1`;
 
 exit 0;
 
