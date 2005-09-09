76cc56959342a25ddb7f79c95e45f365a4f89ac7
joel <joel@5d86edc2-cc7b-4035-901b-baf054620955>
2005-09-09 16:10:48 +0000

    Fixing bug

--- pads/padsc/usecases/simple/src/test_simple.h
+++ pads/padsc/usecases/simple/src/test_simple.h
@@ -1,5 +1,5 @@
 #define DEF_INPUT_FILE "../../data/simple"
-#define DEF_QUERY_FILE "W_D/html/sources/simple/queries/dot.xq"
+#define DEF_QUERY_FILE "../../queries/dot.xq"
 #define PADS_TY_ simple_example
 #define PADS_TY(suf) simple_example ## suf
 #define PPADS_TY(pref) pref ## simple_example
