e05fcd45944a5ab26e77b0fedd073c0b03131aa1
yitzhakm <yitzhakm@5d86edc2-cc7b-4035-901b-baf054620955>
2005-09-30 14:01:37 +0000

    *** empty log message ***

--- pads/padsc/examples/xml/tests/time_walkdoc.sh
+++ pads/padsc/examples/xml/tests/time_walkdoc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #run as: <script_name> data_format <bulk | linear | smart> input_extension
+#measure the time to walk entire document using the pads_dm implementation of the Galax data model.
 
 format=$1
 kind=$2
@@ -14,7 +15,7 @@ echo "$ast_arch chosen as native architecture."
 
 # timing_dir=../timing
 alloc_hint=0
-if [[ $format = dibbler_new ]]; then
+if [[ $format = dibbler_new && $kind = bulk ]]; then
     case $size in
 	10MB) alloc_hint=50000;;
 	20MB) alloc_hint=100000;;
