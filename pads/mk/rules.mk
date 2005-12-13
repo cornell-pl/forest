865e66255b3143d4db3ccd0f6605583cd583c3db
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2005-12-13 06:47:08 +0000

    extra underscore, oops

--- pads/mk/rules.mk
+++ pads/mk/rules.mk
@@ -348,7 +348,7 @@ endif
 #if using gcc and -Wno-long-double is supported, use it
 ifeq ($(PADS_CC_IS_GNU),1)
 NOLONGDOUBLE_TEST = $(shell $(CC) -Wno-long-double $(PADS_HOME)/scripts/dummy.c 2>&1)
-ifeq ($(NOLONGDOUBLE_TEST_),)
+ifeq ($(NOLONGDOUBLE_TEST),)
 CDBGFLAGS += -Wno-long-double
 COPTFLAGS += -Wno-long-double
 endif
