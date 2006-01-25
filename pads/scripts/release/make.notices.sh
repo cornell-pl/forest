dababd6d96379d372b101538bc93a5b17eb64b00
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2006-01-25 00:46:04 +0000

    changes to the packaging to change the license

--- pads/scripts/release/make.notices.sh
+++ pads/scripts/release/make.notices.sh
@@ -18,6 +18,7 @@ pwd
 insertCR scripts/release/sml.files  '(*)' kfisher+gruber+yitzhak
 insertCR scripts/release/caml.files '(*)' mff
 insertCR scripts/release/pads.files '/*/' gruber+kfisher+yitzhak
+insertCR scripts/release/analysis.files '/*/' kfisher+xuan
 insertCR scripts/release/padx.files '/*/' mff+gruber+yitzhak+kfisher
 insertCR scripts/release/sh.files   '#'   kfisher+gruber
 
