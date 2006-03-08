efe39400c7fea84d78865e318131a02bc39ed6fc
gruber <gruber@5d86edc2-cc7b-4035-901b-baf054620955>
2006-03-08 08:00:48 +0000

    oops

--- pads/padsc/libpads/rbuf.c
+++ pads/padsc/libpads/rbuf.c
@@ -253,8 +253,6 @@ RMM_free_rbuf_keep_buf_dbg(RBuf_t *rbuf, void **buf_out, RMM_t **mgr_out,
 void*
 RMM_alloc_unmanaged_buf(RMM_t* mgr, size_t size)
 {
-  void *res;
-
   if (!mgr || !mgr->vm || !mgr->fn) {
     return 0; /* failure */
   }
