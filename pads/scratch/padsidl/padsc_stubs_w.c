61d084d6bceb8e45843b8bcb50c03f4be8844840
yitzhakm <yitzhakm@5d86edc2-cc7b-4035-901b-baf054620955>
2006-06-20 20:11:27 +0000

    Added support for Sfio_t * type

--- pads/scratch/padsidl/padsc_stubs_w.c
+++ pads/scratch/padsidl/padsc_stubs_w.c
@@ -2,6 +2,55 @@
 // in padsc_stubs.c
 
 #include "pads.h"
+
+/* 
+  Specialized conversion functions for IDL types that need to
+  combine functionality of ptr and unique attributes.
+*/
+
+#define ml2c_opt_decl(module,type)\
+void ml2c_ ## module ## _ ## type ## Opt(value input, type ## Opt * output);
+
+#define ml2c_opt(module,type)\
+void ml2c_ ## module ## _ ## type ## Opt(value input, type ## Opt * output)\
+{\
+value in2;\
+  if (input == Val_int(0)) {\
+    (*output) = NULL;\
+  } else {\
+    in2 = Field(input, 0);\
+    camlidl_ml2c_ ## module ## _ ## type (in2, output, (camlidl_ctx)NULL);\
+  }\
+}
+
+#define c2ml_opt_decl(module,type)\
+value c2ml_ ## module ## _ ## type ## Opt( type ## Opt * input);
+
+#define c2ml_opt(module,type)\
+value c2ml_ ## module ## _ ## type ## Opt( type ## Opt * input)\
+{\
+value out;\
+value tmpv;\
+  if ((*input) == NULL) {\
+    out = Val_int(0);\
+  } else {\
+    tmpv = camlidl_c2ml_ ## module ## _ ## type (input, (camlidl_ctx)NULL);\
+    Begin_root(tmpv)\
+      out = camlidl_alloc_small(1, 0);\
+      Field(out, 0) = tmpv;\
+    End_roots();\
+  }\
+  return out;\
+}
+
+#define cnv_opt_decl(module,type)\
+  ml2c_opt_decl(module,type)\
+  c2ml_opt_decl(module,type)
+
+#define cnv_opt(module,type)\
+  ml2c_opt(module,type)\
+  c2ml_opt(module,type)
+
 #include "padsc_stubs.c"
 
 /* Add dummy defintion of P_lib_init as there is no generated code to define this function.
