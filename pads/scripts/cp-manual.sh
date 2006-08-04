e8dea534688fc7c3ab6679d088d5fd158e591cce
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2006-08-04 18:24:46 +0000

    changes related to posting the manual to the web site

--- pads/scripts/cp-manual.sh
+++ pads/scripts/cp-manual.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-echo creating tarball for manual
+echo creating manual
 cd $PADS_HOME/documents/manual
 make
-mkdir temp_manual
-mkdir temp_manual/pictures
-mkdir temp_manual/code
-mkdir temp_manual/hand_code
-for x in `cat take_list`; do cp -r $x temp_manual/$x; done
-tar cvf manual.tar temp_manual 
-gzip manual.tar
-rm -rf temp_manual
+echo moving generated web pages to www.padsproj.org
+scp *.html *.pdf pictures/*.jpg www.padsproj.org:/weblab/services/pads/pads/documents/manual
 
-echo moving tarball to www.padsproj.org
-scp manual.tar.gz www.padsproj.org:~kfisher/manual.tar.gz
-
-rm manual.tar.gz
