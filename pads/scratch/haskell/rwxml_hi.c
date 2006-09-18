2c3685e312ae094fcad714fc412ea0df678342c8
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2006-09-18 21:43:23 +0000

    new binary example: haskell interface files

---
+++ pads/scratch/haskell/rwxml_hi.c
@@ -0,0 +1,6 @@
+#define DEF_INPUT_FILE "CPUTime.hi"
+#define PADS_TY(suf) hi ## suf
+#define IO_DISC_MK P_norec_noseek_make(0)
+#define COPY_STRINGS 1
+#include "hi.h"
+#include "template/read_orig_write_xml.h"
