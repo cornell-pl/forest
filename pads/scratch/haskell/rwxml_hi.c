5ed02c39afd29a0c642738a435053fe0bdaf7b91
yitzhakm <yitzhakm@5d86edc2-cc7b-4035-901b-baf054620955>
2006-09-27 18:04:41 +0000

    Added mask init code.

--- pads/scratch/haskell/rwxml_hi.c
+++ pads/scratch/haskell/rwxml_hi.c
@@ -1,6 +1,23 @@
 #define DEF_INPUT_FILE "CPUTime.hi"
 #define PADS_TY(suf) Hi_t ## suf
 #define IO_DISC_MK P_norec_noseek_make(0)
+
+#define IFACE_TY_VAR  m.decls.decls.iFaceDecl.branches.iFaceId.ty
+#define IFACE_TY_PATH branches.forAllTy.iFaceType
+#define KIND_VAR      IFACE_TY_VAR->branches.forAllTy.iFaceTvBndr.kind
+#define KIND_PATH_ARG    branches.funKindBody.arg
+#define KIND_PATH_RESULT branches.funKindBody.result
+
+#define CUSTOM_MASK_CODE \
+do{\
+PADS_TY(_m_init)(pads, &m, READ_MASK);  \
+P_DynamicMaskInit(IFACE_TY_VAR, IfaceType_t_m, _IfaceType_t_m, READ_MASK,\
+		  IFACE_TY_VAR-> IFACE_TY_PATH);\
+P_DynamicMaskInit(KIND_VAR, Kind_t_m, _Kind_t_m, READ_MASK,KIND_VAR->KIND_PATH_ARG);\
+KIND_VAR->KIND_PATH_RESULT = KIND_VAR;\
+}while(0)
+
 #define COPY_STRINGS 1
 #include "hi.h"
 #include "template/read_orig_write_xml.h"
+
