312fe8aab8d7707a3ac22cf566443732bd691ad4
forrest <forrest@5d86edc2-cc7b-4035-901b-baf054620955>
2006-10-12 18:31:39 +0000

    dibbler_new.h should have been picked up from the gen directory

--- pads/padsc/examples/tests/test_dibbler_new.c
+++ pads/padsc/examples/tests/test_dibbler_new.c
@@ -1,4 +1,4 @@
-// #define DEF_INPUT_FILE "../../data/dibbler.10001"
+#define DEF_INPUT_FILE "../../data/dibbler.10001"
 #define TIMESTAMP_IN_FMT "%s"
 #define TIMESTAMP_OUT_FMT "%s"
 #define PADS_HDR_TY(suf) summary_header_t ## suf
