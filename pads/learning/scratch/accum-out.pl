023b3dbebea48388a323d4cd5874145c659ae02e
kzhu <kzhu@5d86edc2-cc7b-4035-901b-baf054620955>
2007-06-05 01:38:42 +0000

    A simple implementation of end to end testing on training samples. Change the definition of make xxx.rep.

--- pads/learning/scratch/accum-out.pl
+++ pads/learning/scratch/accum-out.pl
@@ -1,15 +1,24 @@
 #!/usr/bin/perl
 
-$accumFile = $ARGV[0];
-open  (ACCUM, $accumFile) || die "Can't open $accumFile";
+@accumFiles = @ARGV;
+$numfiles = $#accumFiles + 1;
+$errrate=0;
+foreach my $accumFile  (@accumFiles)
+{
+  open  (ACCUM, $accumFile) || die "Can't open $accumFile";
 
-while ($record = <ACCUM>) {
-  if ($record =~ /good vals: *([0-9]+) *bad vals: *([0-9]+)/) {
+  while ($record = <ACCUM>) {
+    if ($record =~ /good vals: *([0-9]+) *bad vals: *([0-9]+)/) {
      local ($good, $bad) = ($1, $2);
-     print "\n\nnumber of correctly parsed records = $good\n";
-     print "number of incorrectly parsed records = $bad\n";
+     $newrate = $bad/($good+$bad);
+     print "$accumFile: error rate = $newrate\n";
+     $errrate += $newrate;
+#     print "\n\nnumber of correctly parsed records = $good\n";
+#     print "number of incorrectly parsed records = $bad\n";
      last;
+    }
   }
+  close (ACCUM);
 }
-
-close (ACCUM);
+$errrate = $errrate /$numfiles;
+print "The average error rate = $errrate \n";
