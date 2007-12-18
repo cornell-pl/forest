dfe61eea08f844581da9750545171bb8809a3da5
kfisher <kfisher@5d86edc2-cc7b-4035-901b-baf054620955>
2007-12-18 03:55:29 +0000

    file path change

--- pads/infer/internal/scripts/release/make.notices.sh
+++ pads/infer/internal/scripts/release/make.notices.sh
@@ -8,7 +8,7 @@ do
   for f in `find $p -type f`
   do
 	echo Updating $f
-	proto -l  internal/scripts/infer.lic -hpr -c"$2" -o author=$3 $f
+	proto -l  internal/scripts/release/infer.lic -hpr -c"$2" -o author=$3 $f
   done
 done
 }
