0cc425b86cf204f64769d7d6b351b539c927ca2e
dpw <dpw@5d86edc2-cc7b-4035-901b-baf054620955>
2008-07-28 15:59:27 +0000

    Michel Blockelet, Summer intern at Princeton University in June-July 2008 developed the first implementation of PADS for Haskell under the supervision of David Walker.
    Some of the key design ideas:
    
    * use of built-in GADTs to represent the abstract syntax of PADS
       * interestingly, we considered many options for type-directed
         programming (many different ones exist in Haskell).  We settled
         on using GADTs, forgetting that that is what Yitzhak had done for
         PADS/ML -- that probably means that Yitzhak really made the right
         choice!  Though the implementation in Haskell is trivial compared with
         the trickiness that Yitzhak had to figure out to do the encoding.
    
       * though it probably looks easy now that we've done it (and perhaps
         it was! :-)  The difficult part was coming up with a solution that
         allowed (1) arbitrary data types to be representations (2) dependency
         including dependency on values with arbitrary types (3) annotations
         carrying values with arbitrary types (4) keeping things "well-typed"
         so that the type checker actually enforces useful invariants (eg:
         avoiding universal datatype definitions) (5) making it possible to
         use the representation to write all generic tools
    
       * all tools, including parsers and printers are coded easily using
         induction over the structure of the GADT
    
       * thanks to Stephanie Weirich for discussions about the various options
         we could use and help with GADTs in Haskell
    
    * encoding of arbitrary data types using existential types and lists of
      constructor encodings where each constructor is encoded as a pair of
      injection and projection functions.  Turns out we reinvented an idea
      Stephanie Weirich uses in her RepLib library for generic programming.
    
    * inclusion of "annotations" in the PADS descriptions that can be used to
    customize the actions of particular tools.  These annotations carry values
    of arbitrary types.  Moreover, each new tool can define its own new set of
    annotations.  The implementation technique involves using the type class
    Typeable to simulate an extensible datatype.  To define a new set of
    annotations for a new tool, merely define a new datatype and derive the
    Typeable type class.  In the tool, given an annotation, use type-safe
    cast to determine if the annotation has a type associated with the tool.
    If so, process the annotation however desired.  If not, discard the
    annotation.  Thanks to the Haskell-cafe mailing lists for suggestions
    on how to encode extensible datatypes using Typeable and existential
    types (we figured out the existentials ourselves but weren't aware of
    the existence of Typeable until we asked the Haskell gurus.
    
    * we reimplemented regular expressions and added "tags" as suggested
    by Ville Laurikari:
    
    http://citeseer.ist.psu.edu/350334.html
    
    * we implemented parser, printer, xml generator.  All of these tools
    were more or less trivial to implement once we figured out the right
    representation for the PADS abstract syntax.  There are things we
    need to fix with the tools (eg: the xml pds are done differently
    than the other tools; the parser and error recovery is a mess) but
    this is "just engineering" :-).  Basically, the overall implementation
    strategy and all the typing now "just works".  The features of Haskell
    give a huge leg up on C (obviously!) but also it would appear on O'Caml.
    
    Dave

---
+++ pads/haskell/Gen.hs
@@ -0,0 +1,67 @@
+-- Author: Michel Blockelet (supervised by David Walker)
+-- Institution: Princeton University
+-- Date: July 08
+
+module Gen where
+
+import Data.Typeable
+import System.Random
+
+import Types
+import Aux
+import Parser
+
+
+
+data GenAnnot where
+  IntLimit :: Int -> GenAnnot
+  UserFunc :: ([Int] -> String) -> GenAnnot
+  deriving Typeable
+
+
+{--- GenAnnot lookup functions ---}
+{- ga_intlimit :: limit for Pints -}
+ga_intlimit :: [Annot] -> Int
+ga_intlimit [] = 2^30
+ga_intlimit (x:xs) = case x of Annot y -> case (cast y) of Just (IntLimit n) -> n
+                                                           _ -> ga_intlimit xs
+
+{- ga_userfunc :: user function for generating some part of the description -}
+ga_userfunc :: Annot -> [Int] -> String -> String
+ga_userfunc x rl val = case x of
+                         Annot y -> case (cast x) of
+                                      Just (UserFunc f) -> f rl
+                                      _ -> val
+
+
+
+{--- The generator itself ---}
+do_gen :: [Annot] -> [Int] -> PADS x -> String
+do_gen env (x:rl) Pint = show (mod x (ga_intlimit env))
+do_gen env (x:rl) Pchar = showChar (toEnum (mod x 127)) ""
+do_gen env (x:rl) Pfloat = show ((int_to_float x)/100000)
+do_gen env rl Punit = ""
+do_gen env rl (Pbefore s p) = s ++ (do_gen env rl p)
+do_gen env rl (Pafter p s) = (do_gen env rl p) ++ s
+do_gen env rl (Ppair pa pb) = let (rla, rlb) = splitoae rl in
+                              (do_gen env rla pa) ++ (do_gen env rlb pb)
+do_gen env rl (Pdpair id p f) = let (rla, rlb) = splitoae rl in
+                            let sy = (do_gen env rla p) in
+                            let (py, _, _) = (parser env p sy) in
+                            sy ++ (do_gen env rlb (f py))
+do_gen env (n:rl) (Plist p s) = (foldl (++) "" (map (\x -> do_gen env (takeEvery (mod n 20) rl x) p) [0..((mod n 20)+1)])) ++ s
+do_gen env rl (PlistFW p n) = foldl (++) "" (map (\x -> do_gen env (takeEvery n rl x) p) [0..(n-1)])
+do_gen env rl (Pnamed p n) = do_gen env rl p
+do_gen env rl (Pannot p a) = ga_userfunc a rl (do_gen env rl p)
+do_gen env rl (Pdata l) = case (head l) of Branch ((_, f, _), p) -> do_gen env rl p
+
+
+
+{--- Mix up a string ---}
+str_mixup :: [Int] -> String -> String
+str_mixup (m:n:rl) [] = if (mod m 25) == 0 then showChar (toEnum (mod n 127)) "" else ""
+str_mixup (m:n:rl) (x:xs) = case (mod m 25) of
+                              0 -> (toEnum (mod n 127)):(str_mixup rl xs)
+                              1 -> (toEnum (mod n 127)):x:(str_mixup rl xs)
+                              2 -> str_mixup rl xs
+                              _ -> x:(str_mixup rl xs)
