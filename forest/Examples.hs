bc47807b2c90ddedae6763bf4548c02340678ef7
Hugo Pacheco <hpacheco@gmail.com>
2014-11-24 11:47:46 -0500

    pads-haskell update

---
+++ forest/Examples.hs
@@ -0,0 +1,13 @@
+module Examples where
+
+import Examples.IC.Papers2
+import Examples.IC.Universal
+
+import Examples.Pure.Beautiful.Account
+import Examples.Pure.Coral
+import Examples.Pure.CVS
+import Examples.Pure.GeneAssociation
+import Examples.Pure.Papers2
+import Examples.Pure.PWS
+import Examples.Pure.Students2
+import Examples.Pure.Universal
---
+++ pads-haskell/Examples.hs
@@ -0,0 +1,10 @@
+module Examples where
+
+import Examples.AI
+import Examples.Binary
+import Examples.First
+import Examples.HmmPlus
+import Examples.IntPair
+import Examples.Regression
+import Examples.Small
+import Examples.VCard
